a5ccb58f5d45bab57e5743f039397bc5df783f2f
Cesar Pazzi <csrpazzi@gmail.com>
2024-04-06 13:39:02 -0600

    renombre de inventarios a productos terminados, renombre de archivos y busqueda de exact a contains

--- sfcs/urls.py
+++ sfcs/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 
-    path('inventarios/agregar_registro/', views.inventarios_agregar_registro, name='inventarios_agregar_registro'),
-    path('inventarios/registros/', views.inventarios_registros, name='inventarios_registros'),
-    path('inventarios/registros/<int:pk>', views.inventarios_ver_registro, name='inventarios_registro'),
-    path('inventarios/borrar_registro/<int:pk>', views.inventarios_borrar_registro, name='inventarios_borrar_registro'),
-    path('inventarios/', views.inventarios_home, name='inventarios'),
-    path('inventarios/registros/<int:pk>/editar', views.inventarios_editar_registro, name='inventarios_editar_registro'),
-    path('inventarios/buscar/', views.inventarios_buscar_registro, name='inventarios_buscar_registro'),
+    path('productos_terminados/agregar_registro/', views.productos_terminados_agregar_registro, name='productos_terminados_agregar_registro'),
+    path('productos_terminados/registros/', views.productos_terminados, name='productos_terminados_registros'),
+    path('productos_terminados/registros/<int:pk>', views.productos_terminados_ver_registro, name='productos_terminados_registro'),
+    path('productos_terminados/borrar_registro/<int:pk>', views.productos_terminados_borrar_registro, name='productos_terminados_borrar_registro'),
+    path('productos_terminados/', views.productos_terminados_home, name='productos_terminados'),
+    path('productos_terminados/registros/<int:pk>/editar', views.productos_terminados_editar_registro, name='productos_terminados_editar_registro'),
+    path('productos_terminados/buscar/', views.productos_terminados_buscar_registro, name='productos_terminados_buscar_registro'),
 ]
--- sfcs/views.py
+++ sfcs/views.py
@@ -35,59 +35,59 @@ def logout_user(request):
     return redirect('home')
 
 # VER TODOS LOS REGISTROS
-def inventarios_registros(request):
+def productos_terminados(request):
     if request.user.is_authenticated:
         registros = Almacen.objects.all()
     else:
         messages.error(request, "Debes de iniciar sesion!")
         print ("no ha iniciado sesion para ver esa pagina")
         return redirect('home')
-    return render(request, 'inventarios/registro.html', {'registros': registros})
+    return render(request, 'productos_terminados/registro.html', {'registros': registros})
 
 
 # VER REGISTRO INDIVIDUAL
-def inventarios_ver_registro(request, pk):
+def productos_terminados_ver_registro(request, pk):
     if request.user.is_authenticated:
         registro = Almacen.objects.get(id=pk)
     else:
         messages.error(request, "Debes de iniciar sesion!")
         print ("no ha iniciado sesion para ver esa pagina")
         return redirect('home')
-    return render(request, 'inventarios/ver_registro.html', {'registro': registro})
+    return render(request, 'productos_terminados/ver_registro.html', {'registro': registro})
 
 # BORRAR REGISTRO
-def inventarios_borrar_registro(request, pk):
+def productos_terminados_borrar_registro(request, pk):
     if request.user.is_authenticated:
         registro = Almacen.objects.get(id=pk)
         registro.delete()
         messages.error(request, "Registro eliminado correctamente!")
         print ("registro borrado")
-        return redirect('inventarios_registros')
+        return redirect('productos_terminados')
     else:
         messages.error(request, "Debes de iniciar sesion!")
         print ("no ha iniciado sesion para ver esa pagina")
         return redirect('home')
 
 # CAPTURAR REGISTRO
-def inventarios_agregar_registro(request):
+def productos_terminados_agregar_registro(request):
     form = AddRecordForm(request.POST or None)
     if request.user.is_authenticated:
         if request.method == 'POST':
             if form.is_valid():
                 agregar_registro = form.save()
                 messages.success(request, "Registro agregado correctamente!")
-                return redirect('inventarios_registros')
-        return render(request, 'inventarios/agregar_registro.html', {'form': form})
+                return redirect('productos_terminados')
+        return render(request, 'productos_terminados/agregar_registro.html', {'form': form})
     else:
         messages.error(request, "Debes de iniciar sesion!")
         print ("no se ha iniciado sesion para ver esa pagina")
         return redirect('home')
     
-def inventarios_home(request):
-    return render(request, 'inventarios/inventarios.html', {})
+def productos_terminados_home(request):
+    return render(request, 'productos_terminados/productos_terminados.html', {})
 
 # EDITAR REGISTRO
-def inventarios_editar_registro(request, pk):
+def productos_terminados_editar_registro(request, pk):
     if request.user.is_authenticated:
         registro = Almacen.objects.get(id=pk)
         form = AddRecordForm(request.POST or None, instance=registro)
@@ -95,18 +95,18 @@ def inventarios_editar_registro(request, pk):
             form.save()
             messages.success(request, "Registro editado correctamente!")
             return redirect('../%d' % registro.id)
-        return render(request, 'inventarios/editar_registro.html', {'form':form, 'registro': registro})
+        return render(request, 'productos_terminados/editar_registro.html', {'form':form, 'registro': registro})
     else:
         messages.error(request, "Debes de iniciar sesion!")
         print ("no ha iniciado sesion para ver esa pagina")
         return redirect('home')
     
-def inventarios_buscar_registro(request):
+def productos_terminados_buscar_registro(request):
     if request.method == 'POST':
             busqueda = request.POST['busqueda']
             # busqueda exacta de la busqueda de la descripcion del registro en el modelo
             # https://docs.djangoproject.com/en/4.0/ref/models/querysets/#std-fieldlookup-exact
-            resultados = Almacen.objects.filter(descripcion__exact=busqueda)
-            return render(request, 'inventarios/buscar.html', {'busqueda':busqueda, 'resultados':resultados})
+            resultados = Almacen.objects.filter(descripcion__contains=busqueda)
+            return render(request, 'productos_terminados/buscar.html', {'busqueda':busqueda, 'resultados':resultados})
     else:
-        return render(request, 'inventarios/buscar.html',{})
+        return render(request, 'productos_terminados/buscar.html',{})
